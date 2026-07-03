21af5647cf37f45bfd5fc9dad484ee5c99785b8c
rjtechforce-byte <rjtechforce@gmail.com>
2026-07-03 14:48:58 +0530

    Update API endpoint for starting exam

--- src/certificate/instructions.jsx
+++ src/certificate/instructions.jsx
@@ -27,7 +27,7 @@ const ExamInstructions = () => {
       setLoading(true);
 
       const res = await axios.post(
-        "https://digital-sakhi-backend-4.onrender.com/api/exam/start",
+        "https://digital-sakhi-backend-4-rapv.onrender.com/api/exam/start",
         { userId }
       );
 
